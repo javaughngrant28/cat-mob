abd129692b9518776ddfef5dd28f3c5aa475dc2d
Grant <javaughngrant28@gmail.com>
2023-12-20 09:35:15 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+    './src/**/*.js',
+    // Add more file paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
